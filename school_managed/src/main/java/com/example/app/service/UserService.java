35470056956c47decc68e6d1dd5ef2b7c6fc3213
hashida-yuhei <aso1301261@gmail.com>
2023-04-16 17:36:49 +0900

    Java Persistence API（JPA）でDB取得するひな形作成

---
+++ school_managed/src/main/java/com/example/app/service/UserService.java
@@ -0,0 +1,26 @@
+package com.example.app.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.app.entity.User;
+import com.example.app.repository.UserRepository;
+
+/**
+ * ユーザー情報 Service
+ */
+@Service
+public class UserService {
+	/**
+	 * ユーザー情報 Repository
+	 */
+	@Autowired
+	UserRepository userRepository;
+
+	public List<User> searchAll() {
+		// ユーザーTBLの内容を全検索
+		return userRepository.findAll();
+	}
+}
